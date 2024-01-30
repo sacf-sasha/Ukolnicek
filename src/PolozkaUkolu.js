cf48338a57a71fae01e53eaccb991ff098d48de1
Aliaksandr Zaharouski <aliaksandr.zaharousk@beanstudent.cz>
2024-01-30 12:28:39 +0100

    hotovo stav ukolu

--- src/PolozkaUkolu.js
+++ src/PolozkaUkolu.js
@@ -1,11 +1,13 @@
- export default function PolozkaUkolu({ukol, poradi, SmazatUkol}) {
+ export default function PolozkaUkolu({ukol, poradi, SmazatUkol, fceSpln}) {
     return(
         <>
         <div className="ukol">
             <span>{poradi} </span> 
-            <span style={{color: "blue", fontWeight:"bold" }}>{ukol}</span> 
+            {!ukol.splneno && <span className="textUkolu">{ukol.text}</span>}
+            {ukol.splneno && <span className="splneno">{ukol.text}</span>}
+
             <button onClick={()=> SmazatUkol(poradi)}>smazat</button> 
-            <button>Hotovy</button>
+            <button onClick={()=> fceSpln(poradi, )}>Hotovy</button> 
         </div>
         <br/>
         </>
--- src/Ukolnicek.js
+++ src/Ukolnicek.js
@@ -3,19 +3,35 @@ import {useState} from 'react'
 import PolozkaUkolu from "./PolozkaUkolu";
 
 export default function Ukolnicek() {
-    const [seznamUkolu, nastavSeznamUkolu] = useState([''])
+
+
+
+    const [seznamUkolu, nastavSeznamUkolu] = useState([
+        {text: "Novy ukol", splneno: false},
+        {text: "dalsi ukol", splneno: false},
+        {text: "splneny ukol", splneno: true},
+    ])
     
     function pridejUkol(novyUkol) {
-        nastavSeznamUkolu([...seznamUkolu, novyUkol])
-        
+        let novyUkolObj = {text: novyUkol, splneno: false}
+        nastavSeznamUkolu([...seznamUkolu, novyUkolObj])
     }
     function SmazatUkol(poradiUkolu) {
         console.log("smazat tenhle ukol",poradiUkolu)
         let upravenySeznamUkolu = [...seznamUkolu];
         upravenySeznamUkolu.splice(poradiUkolu,1);
         nastavSeznamUkolu(upravenySeznamUkolu);
-        
     }   
+    function splnUkol(poradiUkolu) {
+        let upravenySeznamUkolu = [...seznamUkolu];
+        upravenySeznamUkolu[poradiUkolu].splneno = !upravenySeznamUkolu[poradiUkolu].splneno
+        // if (upravenySeznamUkolu[poradiUkolu].splneno == true) {
+        //     upravenySeznamUkolu[poradiUkolu].splneno = false
+        // } else{
+        //     upravenySeznamUkolu[poradiUkolu].splneno = true
+        // }
+        nastavSeznamUkolu(upravenySeznamUkolu);
+    }
 
 
     return (
@@ -32,7 +48,7 @@ export default function Ukolnicek() {
         ))
         } */}
         {seznamUkolu.map((ukol, poradi) => 
-        <PolozkaUkolu ukol={ukol} key={poradi} poradi={poradi}SmazatUkol={SmazatUkol}/>)}
+        <PolozkaUkolu ukol={ukol} key={poradi} poradi={poradi}SmazatUkol={SmazatUkol} fceSpln={splnUkol}/>)}
         </div>
         <span>Počet úkolů: {seznamUkolu.length}</span>
         <button onClick={() => pridejUkol("Nový úkol")}>Pridej nahodný ukol</button>
